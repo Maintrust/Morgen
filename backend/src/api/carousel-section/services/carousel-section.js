9687afc2d5bf50164cf200a6d6eda944167a0b0f
Roman Kosolapov <rkosolapov@lincolnlabs.co>
2023-02-13 15:19:36 +0400

    adding backend

---
+++ backend/src/api/carousel-section/services/carousel-section.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * carousel-section service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::carousel-section.carousel-section');
