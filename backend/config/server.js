2112137964a3e38918a7e616d497ac1895bcc23a
Roman Kosolapov <rkosolapov@lincolnlabs.co>
2023-03-04 12:26:32 +0400

    add server configs

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,8 +1,9 @@
 module.exports = ({ env }) => ({
+  url: env('APP_URL'),
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['c70b25fc509f2179', 'd8ef3623739e50a6']),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
