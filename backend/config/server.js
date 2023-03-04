788f69acf4f3ba3241f7d4454614288e09290253
Roman Kosolapov <rkosolapov@lincolnlabs.co>
2023-03-04 12:32:17 +0400

    change env variable name, restore keys

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  url: env('APP_URL'),
+  url: env('MY_HEROKU_URL'),
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS', ['c70b25fc509f2179', 'd8ef3623739e50a6']),
+    keys: env.array('APP_KEYS'),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
