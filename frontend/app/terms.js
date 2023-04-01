8e49885be5baf14620242d9e1d7ae91fe5e04819
Roman Kosolapov <rkosolapov@lincolnlabs.co>
2023-04-01 18:42:49 +0400

    add cms support for terms

--- frontend/app/terms.js
+++ frontend/app/terms.js
@@ -1,10 +1,16 @@
 import React from "react";
 import WebsiteWrapper from "./website-wrapper";
+import useFetch from "../hooks/useFetch";
 
 export default function Terms() {
+  const {loading, error, data} = useFetch('terms');
+  
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :(</p>
+
   return (
     <WebsiteWrapper>
-      Here will be terms
+      { data.content }
     </WebsiteWrapper>
   )
 }
