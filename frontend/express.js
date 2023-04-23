8bbcea75d99de9d53c6853a05f1ae4d31aeb725c
Roman Kosolapov <rkosolapov@lincolnlabs.co>
2023-04-23 15:55:40 +0400

    add non-home pages get requests in express

--- frontend/express.js
+++ frontend/express.js
@@ -5,6 +5,12 @@ const port = process.env.PORT || 3001;
 const publicPath = path.join(__dirname, 'dist');
 app.use(express.static(publicPath));
 
+// handle every other route with index.html, which will contain
+// a script tag to your application's JavaScript file(s).
+app.get('*', function (request, response){
+  response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
+
 app.listen(port, () => {
   console.log(`Server is up on port ${port}!`);
 });
